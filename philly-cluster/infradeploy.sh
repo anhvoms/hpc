c14945f43ae782cda5ae16ea52be8844553f0aba
Anh Vo <anhvo@microsoft.com>
2018-01-03 00:52:39 -0800

    added code to generate cloud config yaml from a template, also generate the machines section for cluster.yml

--- philly-cluster/infradeploy.sh
+++ philly-cluster/infradeploy.sh
@@ -15,118 +15,219 @@ fi
 NAME=$(hostname)
 IP=$(ifconfig eth0 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
 
-NUMNODES=$1
+INFRA_COUNT=$1
 ADMIN_USERNAME=$2
 ADMIN_PASSWORD=$3
 INFRA_BASE_NAME=$4
-IPBASE=$5
-IPSTART=$6
+INFRA_IP_BASE=$5
+INFRA_IP_START=$6
 
 WORKER_BASE_NAME=$7
-WORKERCOUNT=$8
-WORKERIPBASE=$9
-WORKERIPSTART=${10}
+WORKER_COUNT=$8
+WORKER_IP_BASE=$9
+WORKER_IP_START=${10}
 TEMPLATE_BASE=${11}
-
-echo $IPBASE$IPSTART master >> /etc/hosts
-echo $IP $NAME >> /etc/hosts
 
 masterIndex=0
 
-# Don't require password for HPC user sudo
-echo "$ADMIN_USERNAME ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+function initialSetup()
+{
+    # Don't require password for HPC user sudo
+    echo "$ADMIN_USERNAME ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+    
+    # Disable tty requirement for sudo
+    sed -i 's/^Defaults[ ]*requiretty/# Defaults requiretty/g' /etc/sudoers
+    mkdir -p /home/$ADMIN_USERNAME/.ssh
+    mkdir -p ~/.ssh
+    cp /var/lib/philly/bootstrap/.ssh/* /home/$ADMIN_USERNAME/.ssh
+    cp /var/lib/philly/bootstrap/.ssh/* ~/.ssh
+    
+    chmod 700 /home/$ADMIN_USERNAME/.ssh
+    chmod 400 /home/$ADMIN_USERNAME/.ssh/config
+    chmod 640 /home/$ADMIN_USERNAME/.ssh/authorized_keys
+    chmod 400 /home/$ADMIN_USERNAME/.ssh/id_rsa
+    chown -R $ADMIN_USERNAME:$ADMIN_USERNAME /home/$ADMIN_USERNAME/.ssh
+}
 
-# Disable tty requirement for sudo
-sed -i 's/^Defaults[ ]*requiretty/# Defaults requiretty/g' /etc/sudoers
 
-mkdir -p /home/$ADMIN_USERNAME/.ssh
-mkdir -p ~/.ssh
-cp /var/lib/philly/bootstrap/.ssh/* /home/$ADMIN_USERNAME/.ssh
-cp /var/lib/philly/bootstrap/.ssh/* ~/.ssh
+function fixHostsFile()
+{
+    #fix up hosts file
+    echo $INFRA_IP_BASE$INFRA_IP_START master >> /etc/hosts
 
-chmod 700 /home/$ADMIN_USERNAME/.ssh
-chmod 400 /home/$ADMIN_USERNAME/.ssh/config
-chmod 640 /home/$ADMIN_USERNAME/.ssh/authorized_keys
-chmod 400 /home/$ADMIN_USERNAME/.ssh/id_rsa
-chown -R $ADMIN_USERNAME:$ADMIN_USERNAME /home/$ADMIN_USERNAME/.ssh
+    i=0
+    while [ $i -lt $INFRA_COUNT ]
+    do
+        nextip=$((i + INFRA_IP_START))
+        echo $INFRA_IP_BASE$nextip $INFRA_BASE_NAME$i >> /etc/hosts
+        ((++i))
+    done
 
-if [ "$NAME" == "$INFRA_BASE_NAME$masterIndex" ] ; then
-   
-   sudo chmod g-w /var/log
+    i=0
+    while [ $i -lt $WORKER_COUNT ]
+    do
+        nextip=$((i + WORKER_IP_START))
+        echo $WORKER_IP_BASE$nextip $WORKER_BASE_NAME$i >> /etc/hosts
+        ((++i))
+    done
+}
 
-   # Download slurm.conf and fill in the node info
-   SLURMCONF=/tmp/slurm.conf.$$
 
-   wget $TEMPLATE_BASE/slurm.template.conf -O $SLURMCONF
-   sed -i -- 's/__MASTERNODE__/'"$NAME"'/g' $SLURMCONF
+function generateMachinesYml()
+{
+    machineYmlFile="/home/$ADMIN_USERNAME/machines.yml"
+    echo "#This file is generated automatically at provisioning" > $machineYmlFile
 
-   lastvm=`expr $NUMNODES - 1`
-   sed -i -- 's/__WORKERNODES__/'"$INFRA_BASE_NAME"'[1-'"$lastvm"']/g' $SLURMCONF
-   cp -f $SLURMCONF /etc/slurm-llnl/slurm.conf
-   chown slurm /etc/slurm-llnl/slurm.conf
-   chmod o+w /var/spool # Write access for slurmctld log. Consider switch log file to another location
+    {
+    i=0
+    while [ $i -lt $INFRA_COUNT ]
+    do              
+        nextip=$((i + INFRA_IP_START))
+        echo "    $INFRA_BASE_NAME$i:" 
+        echo "      sku: standard-d4s-v3" 
+        echo "      rack: rack0"  
+        echo "      rackLocation: 1" 
+        echo "      outlet: 1.0" 
+        echo "      role:infrastructure" 
+        echo "      mac: 00:00:00:00:00:00" 
+        echo "      ip: $INFRA_IP_BASE$nextip" 
+        echo "      infraId: $i" 
+        if [ $i -lt 3 ]
+        then
+            echo "      yarnNodeId: $((i+1))" 
+        fi
 
-   # Start the master daemon service
-   sudo -u slurm /usr/sbin/slurmctld 
-   munged --force
-   slurmd
+        if [ $i -lt 2 ]
+        then
+            echo "      hdfsNameNodeId: $((i+1))" 
+        fi
+        echo "      os: prod-infra" 
+        ((++i))
+    done
+      
+    i=0
+    while [ $i -lt $WORKER_COUNT ]
+    do
+        nextip=$((i + WORKER_IP_START))
+        echo "    $WORKER_BASE_NAME$i:" 
+        echo "      sku: standard-nc6" 
+        echo "      rack: rack0" 
+        echo "      rackLocation: 1" 
+        echo "      outlet: 1.0" 
+        echo "      role:worker" 
+        echo "      mac: 00:00:00:00:00:00" 
+        echo "      ip: $WORKER_IP_BASE$nextip" 
+        echo "      os: prod-worker" 
+        ((++i))
+    done
+    } >> $machineYmlFile
+}
 
-   #Prepare mungekey
-   mungekey=/tmp/munge.key.$$
-   cp -f /etc/munge/munge.key $mungekey
-   chown $ADMIN_USERNAME $mungekey
 
-   #Looping all other infranodes to setup slurm
-   i=1
-   while [ $i -lt $NUMNODES ]
-   do
-       worker=$INFRA_BASE_NAME$i
+function updateCloudConfigFile()
+{
+    etcdInitialCluster=""
+    i=0
+    while [ $i -lt $INFRA_COUNT ]
+    do
+        nextip=$((i + INFRA_IP_START))
+        etcdInitialCluster="${etcdInitialCluster}${INFRA_BASE_NAME}$i=http://$INFRA_IP_BASE$nextip:7001,"
+        ((++i))
+    done
 
-       echo waiting for $worker to be ready
+    #delete the trailing comma
+    etcdInitialCluster=${etcdInitialCluster%?}
+    
+    #update cloud-config file
+    cp /var/lib/philly/cloud-config.yml /var/lib/philly/cloud-config.yml.orig
 
-       second=0
-       while [ -n "$(echo a|ncat $worker 8090 2>&1)" ]; do
-           ((second += 5))                  
-           sleep 5
-       done
+    sed -i "s/__HOSTNAME__/$NAME/g" /var/lib/philly/cloud-config.yml
+    sed -i "s/__HOSTIP__/$IP/g" /var/lib/philly/cloud-config.yml
+    sed -i "s?__ETCD_INITIAL_CLUSTER__?$etcdInitialCluster?g" /var/lib/philly/cloud-config.yml
+}
 
-       echo $worker is ready after $second seconds of waiting
-       
-       sudo -u $ADMIN_USERNAME scp $mungekey $ADMIN_USERNAME@$worker:/tmp/munge.key
-       sudo -u $ADMIN_USERNAME scp $SLURMCONF $ADMIN_USERNAME@$worker:/tmp/slurm.conf
-       sudo -u $ADMIN_USERNAME scp /tmp/hosts.$$ $ADMIN_USERNAME@$worker:/tmp/hosts
-       sudo -u $ADMIN_USERNAME ssh $ADMIN_USERNAME@$worker << 'ENDSSH1'
-       sudo chmod g-w /var/log
-       sudo cp -f /tmp/munge.key /etc/munge/munge.key
-       sudo chown munge /etc/munge/munge.key
-       sudo chgrp munge /etc/munge/munge.key
-       sudo rm -f /tmp/munge.key
-       sudo /usr/sbin/munged --force # ignore egregrious security warning
-       sudo cp -f /tmp/slurm.conf /etc/slurm-llnl/slurm.conf
-       sudo chown slurm /etc/slurm-llnl/slurm.conf
-       sudo slurmd
+
+function slurmMasterSetup()
+{
+    sudo chmod g-w /var/log
+
+    # Download slurm.conf and fill in the node info
+    SLURMCONF=/tmp/slurm.conf.$$
+
+    wget $TEMPLATE_BASE/slurm.template.conf -O $SLURMCONF
+    sed -i -- 's/__MASTERNODE__/'"$NAME"'/g' $SLURMCONF
+
+    lastvm=$((INFRA_COUNT - 1))
+    sed -i -- 's/__WORKERNODES__/'"$INFRA_BASE_NAME"'[1-'"$lastvm"']/g' $SLURMCONF
+    cp -f $SLURMCONF /etc/slurm-llnl/slurm.conf
+    chown slurm /etc/slurm-llnl/slurm.conf
+    chmod o+w /var/spool # Write access for slurmctld log. Consider switch log file to another location
+
+    # Start the master daemon service
+    sudo -u slurm /usr/sbin/slurmctld 
+    munged --force
+    slurmd
+
+    #Prepare mungekey
+    mungekey=/tmp/munge.key.$$
+    cp -f /etc/munge/munge.key $mungekey
+    chown $ADMIN_USERNAME $mungekey
+    #Looping all other infranodes to setup slurm
+    i=1
+    while [ $i -lt $INFRA_COUNT ]
+    do
+        worker=$INFRA_BASE_NAME$i
+
+        echo waiting for $worker to be ready
+
+        second=0
+        while [ -n "$(echo a|ncat $worker 8090 2>&1)" ]; do
+            ((second += 5))                  
+            sleep 5
+        done
+
+        echo $worker is ready after $second seconds of waiting
+
+        #setting up slurm
+        sudo -u $ADMIN_USERNAME scp $mungekey $ADMIN_USERNAME@$worker:/tmp/munge.key
+        sudo -u $ADMIN_USERNAME scp $SLURMCONF $ADMIN_USERNAME@$worker:/tmp/slurm.conf
+        sudo -u $ADMIN_USERNAME scp /tmp/hosts.$$ $ADMIN_USERNAME@$worker:/tmp/hosts
+        sudo -u $ADMIN_USERNAME ssh $ADMIN_USERNAME@$worker << 'ENDSSH1'
+        sudo chmod g-w /var/log
+        sudo cp -f /tmp/munge.key /etc/munge/munge.key
+        sudo chown munge /etc/munge/munge.key
+        sudo chgrp munge /etc/munge/munge.key
+        sudo rm -f /tmp/munge.key
+        sudo /usr/sbin/munged --force # ignore egregrious security warning
+        sudo cp -f /tmp/slurm.conf /etc/slurm-llnl/slurm.conf
+        sudo chown slurm /etc/slurm-llnl/slurm.conf
+        sudo slurmd
 ENDSSH1
 
-      ((++i))
-   done
-   rm -f $mungekey
-else   
-   i=0
-   while [ $i -lt $NUMNODES ]
-   do
-       nextip=$((i + IPSTART))
-       echo $IPBASE$nextip $INFRA_BASE_NAME$i >> /etc/hosts
-       ((++i))
-   done
-   ncat -v -l 8090
-fi
+        ((++i))
+    done
+    rm -f $mungekey
+}
 
-i=0
-while [ $i -lt $WORKERCOUNT ]
-do
-   nextip=$((i + WORKERIPSTART))
-   echo $WORKERIPBASE$nextip $WORKER_BASE_NAME$i >> /etc/hosts
-   ((++i))
-done
+
+function slurmSlaveSetup()
+{
+    ncat -v -l 8090
+}
+
+
+#
+# Main script body
+#
+initialSetup
+fixHostsFile
+generateMachinesYml
+updateCloudConfigFile
+
+if [ "$NAME" == "$INFRA_BASE_NAME$masterIndex" ] ; then  
+    slurmMasterSetup   
+else
+    slurmSlaveSetup
+fi
 
 exit 0
