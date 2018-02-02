0c8e0d60f110eeb3a7b2bd64240d930271f6923f
Anh Vo <anhvo@microsoft.com>
2018-02-02 12:50:15 -0800

    support large disk

--- storage-perf/scripts/disktest.sh
+++ storage-perf/scripts/disktest.sh
@@ -9,14 +9,58 @@ if [ ! -d "/datadisk" ]; then
 	apt-get -y update > /dev/null
 	apt-get --no-install-recommends -y install mdadm > /dev/null
 
-	for i in `seq 1 $6`; do
+#	for i in `seq 1 $6`; do
 		
-			(echo n; echo p; echo 1; echo; echo; echo w) | fdisk /dev/sd${dl:$i-1:1} > /dev/null 
-			part="$part /dev/sd${dl:$i-1:1}1"
+#			(echo n; echo p; echo 1; echo; echo; echo w) | fdisk /dev/sd${dl:$i-1:1} > /dev/null 
+#			part="$part /dev/sd${dl:$i-1:1}1"
 		
+#	done
+
+	declare -a data_disks
+	all_disks=($(lsblk -l -d -n -p -I 8,65,66,67,68 -o NAME))
+	for disk in "${all_disks[@]}"; do
+		# ignore os and ephemeral disks
+		if [ $disk != "/dev/sda" ] && [ $disk != "/dev/sdb" ]; then
+			data_disks=("${data_disks[@]}" "$disk")
+		fi
+	done
+	unset all_disks
+	numdisks=${#data_disks[@]}
+	echo "found $numdisks data disks: ${data_disks[@]}"
+
+	# check if data disks are already partitioned
+	declare -a skipped_part
+	for disk in "${data_disks[@]}"; do
+		part1=$(partprobe -d -s $disk | cut -d' ' -f4)
+		if [ -z $part1 ]; then
+			echo "$disk: partition 1 not found. Partitioning $disk."
+			parted -a opt -s $disk mklabel gpt mkpart primary 0% 100%
+			part1=$(partprobe -d -s $disk | cut -d' ' -f4)
+			if [ -z $part1 ]; then
+				echo "$disk: partition 1 not found after partitioning."
+				exit 1
+			fi
+			# wait for block device
+			wait_for_device $disk$part1
+		else
+			echo "$disk: partition 1 found. Skipping partitioning."
+			skipped_part=("${skipped_part[@]}" "$disk")
+		fi
 	done
 
-	mdadm --create /dev/md1 --level 0 --raid-devices $6 $part > /dev/null
+    declare -a raid_array
+    declare -a all_raid_disks
+    set +e
+    for disk in "${data_disks[@]}"; do
+        mdadm --examine "${disk}1"
+        if [ $? -ne 0 ]; then
+            raid_array=("${raid_array[@]}" "${disk}1")
+        fi
+        all_raid_disks=("${all_raid_disks[@]}" "${disk}1")
+    done
+    set -e
+
+	mdadm --create /dev/md1 --level 0 --raid-devices $numdisks ${raid_array[@]} > /dev/null
 	
 	mkfs -t ext4 /dev/md1 > /dev/null 
 	
